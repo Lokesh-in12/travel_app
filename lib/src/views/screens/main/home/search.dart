7c3ffcdbedf9e040dded8616dd7389086ce4fd62
Lokesh-in12 <fixpoint.lokesh@gmail.com>
2023-04-05 19:09:32 +0530

    fixed release mode grey screen error

--- lib/src/views/screens/main/home/search.dart
+++ lib/src/views/screens/main/home/search.dart
@@ -27,76 +27,74 @@ class _SearchScreenState extends State<SearchScreen> {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Search",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                onChanged: (value) => hotelsController.handleSeachHotel(),
-                controller: hotelsController.searchController,
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(
-                      CupertinoIcons.search,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Search",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              onChanged: (value) => hotelsController.handleSeachHotel(),
+              controller: hotelsController.searchController,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    CupertinoIcons.search,
+                  ),
+                  prefixIconColor: black,
+                  focusColor: black,
+                  fillColor: lightWhite,
+                  filled: true,
+                  hintText: "Search something...",
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 0, color: white)),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0, color: white),
+                  ),
+                  focusedBorder:
+                      OutlineInputBorder(borderSide: BorderSide(color: black))),
+            ),
+            Obx(() {
+              if (kDebugMode) {
+                print(
+                    // ignore: invalid_use_of_protected_member
+                    "toalDLength => "
+                    // ignore: invalid_use_of_protected_member
+                    "serachAray is = > ${hotelsController.SearchHotels.value} and length of controller is => ${hotelsController.searchController.text.trim().length}");
+              }
+              if (hotelsController.SearchHotels.isEmpty ||
+                  hotelsController.searchController.text.trim().isEmpty) {
+                return SizedBox(
+                  height: 500,
+                  child: Center(
+                    child: Text(
+                      "Search through our best Hotels",
+                      style: Theme.of(context).textTheme.titleMedium,
                     ),
-                    prefixIconColor: black,
-                    focusColor: black,
-                    fillColor: lightWhite,
-                    filled: true,
-                    hintText: "Search something...",
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 0, color: white)),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 0, color: white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: black))),
-              ),
-              Obx(() {
-                if (kDebugMode) {
-                  print(
-                      // ignore: invalid_use_of_protected_member
-                      "toalDLength => "
-                      // ignore: invalid_use_of_protected_member
-                      "serachAray is = > ${hotelsController.SearchHotels.value} and length of controller is => ${hotelsController.searchController.text.trim().length}");
-                }
-                if (hotelsController.searchController.text.trim().isEmpty) {
-                  return SizedBox(
-                    height: 500,
-                    child: Center(
-                      child: Text(
-                        "Search through our best Hotels",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                    ),
-                  );
-                }
-                return SingleChildScrollView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: hotelsController.SearchHotels.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 0.72,
-                            mainAxisSpacing: 20,
-                            crossAxisSpacing: 10),
-                    itemBuilder: (BuildContext context, int index) {
-                      return LocationCard(
-                          e: hotelsController.SearchHotels[index]);
-                    },
                   ),
                 );
-              })
-            ],
-          ),
+              }
+              return SingleChildScrollView(
+                physics: const NeverScrollableScrollPhysics(),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: hotelsController.SearchHotels.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.72,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 10),
+                  itemBuilder: (BuildContext context, int index) {
+                    return LocationCard(
+                        e: hotelsController.SearchHotels[index]);
+                  },
+                ),
+              );
+            })
+          ],
         ),
       )),
     );
